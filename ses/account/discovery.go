306fed316f92fe71d07f4a216564b96289b2c56e
youyo <1003ni2@gmail.com>
2021-02-19 14:26:03 +0900

    add ses support regions

--- ses/account/discovery.go
+++ ses/account/discovery.go
@@ -17,12 +17,29 @@ type (
 
 func buildDiscoveryData(zabbixHostGroup, region string) (discoveryData DiscoveryData) {
 	var discoveryItems DiscoveryItems
-	if region == "us-east-1" || region == "us-west-2" || region == "eu-west-1" {
+
+	regions := [18]string{
+		"us-east-1", "us-east-2",
+		"us-west-1", "us-west-2",
+		"ap-south-1",
+		"ap-southeast-1", "ap-southeast-2",
+		"ap-northeast-1", "ap-northeast-2",
+		"ca-central-1",
+		"eu-central-1",
+		"eu-west-1", "eu-west-2", "eu-west-3",
+		"eu-north-1",
+		"me-south-1",
+		"sa-east-1",
+		"us-gov-west-1",
+	}
+
+	for _, region := range regions {
 		discoveryItems = append(discoveryItems, DiscoveryItem{
 			ZabbixHostId:    zabbixHostGroup + "-" + region,
 			ZabbixHostGroup: zabbixHostGroup + "-" + region,
 		})
 	}
+
 	discoveryData = DiscoveryData{discoveryItems}
 	return
 }
